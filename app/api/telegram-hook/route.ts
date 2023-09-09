299baed8d0570b9911a732c17badff8002c0352a
Roman Chasovitin <c-r.chasovitin@exparte.com>
2023-09-09 23:37:07 +0600

    Added post request

--- app/api/telegram-hook/route.ts
+++ app/api/telegram-hook/route.ts
@@ -2,7 +2,13 @@ import { handlePollAnswer } from "@/telegram/handlePollAnswer";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  console.log('REQUEST', request)
+  console.log('GET REQUEST', request)
+  await handlePollAnswer()
+  return NextResponse.json({ ok: true })
+}
+
+export async function POST(request: Request) {
+  console.log('POST REQUEST', request)
   await handlePollAnswer()
   return NextResponse.json({ ok: true })
 }
